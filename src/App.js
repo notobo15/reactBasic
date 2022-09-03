1407c3e80edb663bc4c4736714898829cdad4c5a
notobo15 <binh15122003@gmail.com>
2022-09-03 20:37:14 +0700

    hihii

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident libero quibusdam ipsam omnis delectus, laborum saepe autem illum expedita! Animi.
-      <p></p>
+      </p>
     </div>
   );
 }
